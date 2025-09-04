66fc707a5f66b3e54e3ffa33219ea2a037930ad5
Lasserich <lasse.friedrich@sap.com>
2025-09-04 11:55:05 +0200

    fix: test

--- provider-test.sh
+++ provider-test.sh
@@ -204,8 +204,8 @@ upgrade-test)
     if [ $test_result -ne 0 ]; then
         print_color_message ${RED} "*****provider $PROVIDER_NAME CR resources can not be all applied at source version $SOURCE_VERSION, stop tests."
         # Cleanup resources before exiting
-        cleanup_resources "${SOURCE_DIR_BASENAME}"
         kubectl describe ServiceManager upgrade-test-subaccount-service-manager
+        cleanup_resources "${SOURCE_DIR_BASENAME}"
         exit 1
     fi
 
